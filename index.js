ee9310146d8609974367c16ef6a90d6d99c12d52
Joe Rehfuss <joe.rehfuss@gmail.com>
2019-02-01 13:51:08 -0800

    database setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-// const db = require('./models')
+const db = require('./models');
+const mongoose = require('mongoose');
 // const exphbs = require('express-handlebars');
 const PORT = process.env.PORT || 3000;
 require('dotenv').config();
@@ -10,10 +11,52 @@ require('dotenv').config();
  
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+mongoose.connect("mongodb://localhost/beefbeefbeefyboiz", { useNewUrlParser: true });
 // app.use('/', express.static(__dirname + '/static'));
  
 app.get('/',(req,res)=>{
      res.send('hello world');
+})
+
+app.get('/episodes',(req,res)=>{
+     db.Episode.find({}).then(data=>{
+         res.send(data);
+     })
+})
+
+app.post('/episodes', (req,res)=>{
+    if(req.body.password === process.env.PASSWORD) {
+        db.Episode.create({
+            title:req.body.title,
+            date:new Date(),
+            body: req.body.body,
+            url: req.body.url
+        }).then(data=>{
+            res.send(data)
+        })
+    }
+    else res.send('incorrect password');
+})
+
+app.get('/reviews',(req,res)=>{
+    db.Review.find({}).then(data=>{
+        res.send(data);
+    })
+})
+
+app.post('/reviews', (req,res)=>{
+    if(req.body.password === process.env.PASSWORD) {
+        db.Review.create({
+            title:req.body.title,
+            date: new Date(),
+            body: req.body.body,
+            author: req.body.author,
+            score:req.body.score
+        }).then(data=>{
+            res.send(data)
+        })
+    }
+    else res.send('incorrect password');
 })
 
 app.post('/', (req,res)=>{
